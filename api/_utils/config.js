89b5fc67ae410ca7994b40d9db3372462a0b3b8e
Adnan Kamili <adnan.kamili@gmail.com>
2022-10-27 21:37:16 +0530

    updated env

--- api/_utils/config.js
+++ api/_utils/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     apiBaseUrl: "https://api.cryptlex.com/v3",
 	accessToken: process.env.CRYPTLEX_ACCESS_TOKEN,
-	productId: "PASTE_PRODUCT_ID"
+	productId: process.env.PRODUCT_ID
 }
