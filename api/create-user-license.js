30eff5d7cf5b5f299005f9428c8c1d74504e5c97
Ahmad Kemsan <ahmad.kemsan@cryptlex.com>
2022-11-07 18:10:09 +0530

    refactor(create-user-license): added check for subscription

--- api/create-user-license.js
+++ api/create-user-license.js
@@ -7,49 +7,56 @@ const productId = config.productId;
 module.exports = async (req, res) => {
     let event = req.body;
 
-    if( event == null || event.type == null) {
-        return res.status(400).json({"error" : "Invalid request!"});
+    if (event == null || event.type == null) {
+        return res.status(400).json({ "error": "Invalid request!" });
     }
 
     switch (event.type) {
         case 'invoice.paid':
             break;
         default:
-            return res.json({ event : event.type});
+            return res.json({ event: event.type });
     }
 
-    // get post params from request body
-    const customerName = event.data.object.customer_name;
+    const requestBody = event.data.object
+    if (requestBody.status = "paid" && requestBody.lines.data[0].type == "subscription") {
 
-    const email = event.data.object.customer_email;
-    let firstName = 'John', lastName = '_';
-    if (customerName != "") {
-        const nameParts = customerName.split(' ');
-        firstName = nameParts[0];
-        if (nameParts.length > 1) {
-            lastName = nameParts[1];
+        // get post params from request body
+        const customerName = requestBody.customer_name
+
+        const email = event.data.object.customer_email;
+        let firstName = 'John', lastName = '_';
+        if (customerName != "") {
+            const nameParts = customerName.split(' ');
+            firstName = nameParts[0];
+            if (nameParts.length > 1) {
+                lastName = nameParts[1];
+            }
         }
+        const subscriptionId = event.data.object.subscription;
+
+        // create license user
+        const userBody = {
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: uuidv4(),
+            role: 'user'
+        };
+        const user = await CryptlexApi.createUser(userBody);
+
+        // create license
+        const licenseBody = {
+            productId: productId,
+            userId: user.id,
+            metadata: []
+        };
+        licenseBody.metadata.push({ key: 'subscription_id', value: subscriptionId, visible: false });
+        const license = await CryptlexApi.createLicense(licenseBody);
+        console.log("License created successfully!");
+        res.json({ key: license.key });
     }
-    const customerId = event.data.object.customer;
-
-    // create license user
-    const userBody = {
-        email: email,
-        firstName: firstName,
-        lastName: lastName,
-        password: uuidv4(),
-        role: 'user'
-    };
-    const user = await CryptlexApi.createUser(userBody);
-
-    // create license
-    const licenseBody = {
-        productId: productId,
-        userId: user.id,
-        metadata: []
-    };
-    licenseBody.metadata.push({ key: 'customer_id', value: customerId, visible: false });
-    const license = await CryptlexApi.createLicense(licenseBody);
-    console.log("License created successfully!");
-    res.json({ key: license.key });
+    else {
+        return res.json({ "error": "Invalid event!" });
+    }
 }
