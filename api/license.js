6eeafe327e53d9ce9d0a008c27528aeb56569d0b
Ahmad Kemsan <ahmad.kemsan@cryptlex.com>
2022-11-09 12:13:31 +0530

    refactor(try-catch): added try catch in license script

--- api/license.js
+++ api/license.js
@@ -5,66 +5,76 @@ const config = require('./_utils/config.js');
 const productId = config.productId;
 
 module.exports = async (req, res) => {
-    let event = req.body;
-
-    if (event == null || event.type == null) {
-        return res.status(400).json({ "error": "Invalid request!" });
-    }
-
-    switch (event.type) {
-        case 'invoice.paid':
-            break;
-        default:
-            return res.json({ event: event.type });
-    }
-
-    const invoice = event.data.object
-    if (invoice.status == "paid" && invoice.billing_reason == "subscription_create") {
-
-        // get post params from request body
-        const customerName = invoice.customer_name
-
-        const email = event.data.object.customer_email;
-        let firstName = 'John', lastName = '_';
-        if (customerName != "") {
-            const nameParts = customerName.split(' ');
-            firstName = nameParts[0];
-            if (nameParts.length > 1) {
-                lastName = nameParts[1];
-            }
+    try {
+        let event = req.body;
+        if (event == null || event.type == null) {
+            return res.status(400).json({ "error": "Invalid request!" });
         }
-        const subscriptionId = event.data.object.subscription;
 
-        // create license user
-        const userBody = {
-            email: email,
-            firstName: firstName,
-            lastName: lastName,
-            password: uuidv4(),
-            role: 'user'
-        };
-        const user = await CryptlexApi.createUser(userBody);
+        switch (event.type) {
+            case 'invoice.paid':
+                break;
+            default:
+                return res.json({ event: event.type });
+        }
 
-        // create license
-        const licenseBody = {
-            productId: productId,
-            userId: user.id,
-            metadata: []
-        };
-        licenseBody.metadata.push({ key: 'subscription_id', value: subscriptionId, visible: false });
-        const license = await CryptlexApi.createLicense(licenseBody);
-        console.log("License created successfully!");
-        res.json({ key: license.key });
+        const invoice = event.data.object
+        if (invoice.status == "paid" && invoice.billing_reason == "subscription_create") {
+
+            // get post params from request body
+            const customerName = invoice.customer_name
+
+            const email = event.data.object.customer_email;
+            let firstName = 'John', lastName = '_';
+            if (customerName != "") {
+                const nameParts = customerName.split(' ');
+                firstName = nameParts[0];
+                if (nameParts.length > 1) {
+                    lastName = nameParts[1];
+                }
+            }
+            const subscriptionId = event.data.object.subscription;
+
+            // create license user
+            const userBody = {
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                password: uuidv4(),
+                role: 'user'
+            };
+            const user = await CryptlexApi.createUser(userBody);
+
+            // create license
+            const licenseBody = {
+                productId: productId,
+                userId: user.id,
+                metadata: []
+            };
+            licenseBody.metadata.push({ key: 'subscription_id', value: subscriptionId, visible: false });
+            const license = await CryptlexApi.createLicense(licenseBody);
+            console.log("License created successfully!");
+            res.json({ key: license.key });
+        }
+        else if (invoice.status == "paid" && invoice.billing_reason == "subscription_cycle") {
+
+            // renew license expiry
+            const license = await CryptlexApi.renewLicense(productId, 'subscription_id', invoice.subscription);
+
+            // return new expiry date
+            res.json({ message: `License new expiry date: ${license.expiresAt}` });
+        }
+        else {
+            return res.status(200).json({ event: event.type, status: invoice.status, billingReason: invoice.billing_reason });
+        }
     }
-    else if (invoice.status == "paid" && invoice.billing_reason == "subscription_cycle") {
-
-        // renew license expiry
-        const license = await CryptlexApi.renewLicense(productId, 'subscription_id', invoice.subscription);
-
-        // return new expiry date
-        res.json({ message: `License new expiry date: ${license.expiresAt}` });
-    }
-    else {
-        return res.status(400).json({ event: event.type });
+    catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        }
     }
 }
